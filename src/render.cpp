9d6a9c24d4fab38379d696085e31cc038e602e17
Cardboardflavouredbox <minjoon0901@naver.com>
2026-03-09 13:36:33 +0000

    some lil fixes

--- src/render.cpp
+++ src/render.cpp
@@ -9,5 +9,7 @@ void render() {
   SDL_SetRenderTarget(Global->renderer, Global->render_target);
   SDL_SetRenderDrawColorFloat(Global->renderer, 0, 1, 0, 1);
   SDL_RenderLine(Global->renderer, 0, 0, 1, 1);
+  SDL_SetRenderTarget(Global->renderer, NULL);
+  SDL_RenderTexture(Global->renderer,Global->render_target,NULL,NULL);
   SDL_RenderPresent(Global->renderer);
 }
