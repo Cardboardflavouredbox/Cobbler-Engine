268dc4fdae5f0bded23fac9d1c24677372f8aa9b
Cardboardflavouredbox <minjoon0901@naver.com>
2026-03-19 16:41:43 +0900

    whoops quicker fix

--- include/entity.h
+++ include/entity.h
@@ -8,7 +8,7 @@ struct Entity {
   Vector3 position, moveVector3 = Vector3({0, 0, 0}),
                     velocityVector3 = Vector3({0, 0, 0});
   float gravity = 9.81f * 8.f;
-  float jumpheight = 32, walkspeed = 10.f, runspeed = 1.75f;
+  float jumpheight = 24, walkspeed = 10.f, runspeed = 1.75f;
   Capsule hitbox;
   bool IsGrounded = false;
 };
