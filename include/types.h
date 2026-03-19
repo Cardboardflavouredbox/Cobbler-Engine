bb5dd612136893a96b8a784e3dc4b3214a009864
Cardboardflavouredbox <minjoon0901@naver.com>
2026-03-19 13:33:09 +0900

    new function

--- include/types.h
+++ include/types.h
@@ -12,3 +12,7 @@ Vector3 multiplyVec3(Vector3 inputvec2, float multvalue);
 Vector3 Vector3Normalize(Vector3 input);
 
 Vector3 addVec3(Vector3 input1, Vector3 input2);
+
+float Vector3Dot(Vector3 P1, Vector3 P2);
+
+Vector3 subVec3(Vector3 input1, Vector3 input2);
--- src/update.cpp
+++ src/update.cpp
@@ -8,6 +8,49 @@
 #include "extern.h"
 #include "global.h"
 
+Vector3 closestPointTriangle(Vector3 p, Vector3 a, Vector3 b, Vector3 c) {
+  const Vector3 ab = subVec3(b, a);
+  const Vector3 ac = subVec3(c, a);
+  const Vector3 ap = subVec3(p, a);
+
+  const float d1 = Vector3Dot(ab, ap);
+  const float d2 = Vector3Dot(ac, ap);
+  if (d1 <= 0.f && d2 <= 0.f) return a;  // #1
+
+  const Vector3 bp = subVec3(p, b);
+  const float d3 = Vector3Dot(ab, bp);
+  const float d4 = Vector3Dot(ac, bp);
+  if (d3 >= 0.f && d4 <= d3) return b;  // #2
+
+  const Vector3 cp = subVec3(p, c);
+  const float d5 = Vector3Dot(ab, cp);
+  const float d6 = Vector3Dot(ac, cp);
+  if (d6 >= 0.f && d5 <= d6) return c;  // #3
+
+  const float vc = d1 * d4 - d3 * d2;
+  if (vc <= 0.f && d1 >= 0.f && d3 <= 0.f) {
+    const float v = d1 / (d1 - d3);
+    return addVec3(a, multiplyVec3(ab, v));  // #4
+  }
+
+  const float vb = d5 * d2 - d1 * d6;
+  if (vb <= 0.f && d2 >= 0.f && d6 <= 0.f) {
+    const float v = d2 / (d2 - d6);
+    return addVec3(a, multiplyVec3(ac, v));  // #5
+  }
+
+  const float va = d3 * d6 - d5 * d4;
+  if (va <= 0.f && (d4 - d3) >= 0.f && (d5 - d6) >= 0.f) {
+    const float v = (d4 - d3) / ((d4 - d3) + (d5 - d6));
+    return addVec3(b, multiplyVec3(subVec3(c, b), v));  // #6
+  }
+
+  const float denom = 1.f / (va + vb + vc);
+  const float v = vb * denom;
+  const float w = vc * denom;
+  return addVec3(a, addVec3(multiplyVec3(ab, v), multiplyVec3(ac, w)));  // #0
+}
+
 void playermovement() {
   SDL_GetRelativeMouseState(&P1Inputs->Mouse.x, &P1Inputs->Mouse.y);
   Camera->dir.x += -2 * P1Inputs->Mouse.x;
