796aa8df466d72d8a2a66dc1fa005db8b49e562e
Cardboardflavouredbox <minjoon0901@naver.com>
2026-03-22 13:11:05 +0900

    what

--- src/update.cpp
+++ src/update.cpp
@@ -8,12 +8,61 @@
 #include "extern.h"
 #include "global.h"
 
-Vector2 SATprojectionthing() { return Vector2({0, 0}); }
+Vector2 SAT3Dto2D(int normal, Vector3 point) {
+  switch (normal) {
+    case 0: {  //(1,0,0)
+      return Vector2({point.y, point.z});
+      break;
+    }
+    case 1: {  //(0,1,0)
+      return Vector2({point.x, point.z});
+      break;
+    }
+    case 2: {  //(0,0,1)
+      return Vector2({point.x, point.y});
+      break;
+    }
+  }
+  return Vector2({0, 0});
+}
+
+float SAT2Dtofloat(int normal, Vector2 point) {
+  if (normal == 0)
+    return point.x;
+  else
+    return point.y;
+}
 
 bool SATcubetri(Vector3 aabb[], Vector3 tri[]) {
-  Vector3 normal[3] = {Vector3({1, 0, 0}), Vector3({0, 1, 0}),
-                       Vector3({0, 0, 1})};
   for (int i = 0; i < 3; i++) {
+    Vector2 tempsquare[2] = {SAT3Dto2D(i, aabb[0]), SAT3Dto2D(i, aabb[1])},
+            temptriangle[3] = {SAT3Dto2D(i, tri[0]), SAT3Dto2D(i, tri[1]),
+                               SAT3Dto2D(i, tri[2])};
+    for (int j = 0; j < 2; j++) {
+      float square[2] = {SAT2Dtofloat(j, tempsquare[0]),
+                         SAT2Dtofloat(j, tempsquare[1])},
+            triangle[2] = {SAT2Dtofloat(j, temptriangle[0]),
+                           SAT2Dtofloat(j, temptriangle[1])};
+      if (square[0] > square[1]) {
+        float temp = square[0];
+        square[0] = square[1];
+        square[1] = temp;
+      }
+
+      if (triangle[0] > triangle[1]) {
+        float temp = triangle[0];
+        triangle[0] = triangle[1];
+        triangle[1] = temp;
+      }
+
+      float trithing = SAT2Dtofloat(j, temptriangle[2]);
+      if (trithing < triangle[0])
+        triangle[0] = trithing;
+      else if (trithing > triangle[1])
+        triangle[1] = trithing;
+
+      if (square[0] > triangle[1] || square[1] < triangle[0]) return false;
+    }
   }
   return true;
 }
@@ -84,6 +133,27 @@ float Capsulecollisioncheck(Capsule hitbox, Vector3 position) {
   return dist;
 }
 
+bool movecollisioncheck(Capsule hitbox, Vector3 startposition,
+                        Vector3 endposition) {  // true if collided
+  if (Capsulecollisioncheck(hitbox, startposition) >
+          hitbox.radius - hitbox.reduction ||
+      Capsulecollisioncheck(hitbox, endposition) >
+          hitbox.radius - hitbox.reduction)
+    return true;
+  Vector3 aabb[2] = {
+      addVec3(startposition,
+              Vector3({hitbox.radius, hitbox.height / 2, hitbox.radius})),
+      addVec3(endposition,
+              Vector3({-hitbox.radius, -hitbox.height / 2, -hitbox.radius}))};
+  for (int i = 0; i < Global->mapfaces.size(); i++) {
+    Vector3 triangle[3] = {Global->Points[Global->mapfaces[i].points[0]],
+                           Global->Points[Global->mapfaces[i].points[1]],
+                           Global->Points[Global->mapfaces[i].points[2]]};
+    if (SATcubetri(aabb, triangle)) return true;
+  }
+  return false;
+}
+
 void playermovement() {
   SDL_GetRelativeMouseState(&P1Inputs->Mouse.x, &P1Inputs->Mouse.y);
   Camera->dir.x += -2 * P1Inputs->Mouse.x;
@@ -135,22 +205,22 @@ void EntityMove(Entity* tempentity) {
       Global->deltaTime);
   Vector3 tempposition = tempentity->position, moveresult = Vector3({0, 0, 0});
   tempposition.x += tempmove.x;
-  float distance = Capsulecollisioncheck(tempentity->hitbox, tempposition);
-  if (distance > tempentity->hitbox.radius - tempentity->hitbox.reduction) {
+  if (!movecollisioncheck(tempentity->hitbox, tempentity->position,
+                          tempposition)) {
     moveresult.x += tempmove.x;
   }
 
   tempposition = tempentity->position;
   tempposition.y += tempmove.y;
-  distance = Capsulecollisioncheck(tempentity->hitbox, tempposition);
-  if (distance > tempentity->hitbox.radius - tempentity->hitbox.reduction) {
+  if (!movecollisioncheck(tempentity->hitbox, tempentity->position,
+                          tempposition)) {
     moveresult.y += tempmove.y;
   }
 
   tempposition = tempentity->position;
   tempposition.z += tempmove.z;
-  distance = Capsulecollisioncheck(tempentity->hitbox, tempposition);
-  if (distance > tempentity->hitbox.radius - tempentity->hitbox.reduction) {
+  if (!movecollisioncheck(tempentity->hitbox, tempentity->position,
+                          tempposition)) {
     tempentity->IsGrounded = false;
     moveresult.z += tempmove.z;
   } else {
