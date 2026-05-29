44a0102c7efd645794e46f0324f4da3ebbc29f48
Cardboardflavouredbox <minjoon0901@naver.com>
2026-05-29 15:55:08 +0900

    physics actually work now even though the way it does is kinda horrible

--- Core/physics.cpp
+++ Core/physics.cpp
@@ -1,7 +1,5 @@
 #include "physics.h"
 
-#include <SDL3/SDL_log.h>
-
 #include <glm/glm.hpp>
 
 // https://gamedev.stackexchange.com/a/5589
@@ -55,22 +53,60 @@ bool RayTriCheck(glm::vec3 P1, glm::vec3 P2, glm::vec3 P3, glm::vec3 R1,
   return true;
 }
 
-float getdistancething(glm::vec3 P) {
-  glm::vec3 p1 = P - Camera->position;
-  float ps = std::sin(Camera->dir.x * PI / 180.f);
-  float pc = std::cos(Camera->dir.x * PI / 180.f);
-  glm::quat q =
-      glm::angleAxis(glm::radians(Camera->dir.y), glm::vec3(-pc, -ps, 0.0f));
+// https://stackoverflow.com/a/74395029
+glm::vec3 closestPointTriangle(glm::vec3 p, glm::vec3 a, glm::vec3 b,
+                               glm::vec3 c) {
+  glm::vec3 ab = b - a;
+  glm::vec3 ac = c - a;
+  glm::vec3 ap = p - a;
 
-  p1 = q * p1;
-  return p1.y * pc - p1.x * ps;
+  float d1 = dot(ab, ap);
+  float d2 = dot(ac, ap);
+  if (d1 <= 0.f && d2 <= 0.f) return a;  // #1
+
+  glm::vec3 bp = p - b;
+  float d3 = dot(ab, bp);
+  float d4 = dot(ac, bp);
+  if (d3 >= 0.f && d4 <= d3) return b;  // #2
+
+  glm::vec3 cp = p - c;
+  float d5 = dot(ab, cp);
+  float d6 = dot(ac, cp);
+  if (d6 >= 0.f && d5 <= d6) return c;  // #3
+
+  float vc = d1 * d4 - d3 * d2;
+  if (vc <= 0.f && d1 >= 0.f && d3 <= 0.f) {
+    float v = d1 / (d1 - d3);
+    return a + v * ab;  // #4
+  }
+
+  float vb = d5 * d2 - d1 * d6;
+  if (vb <= 0.f && d2 >= 0.f && d6 <= 0.f) {
+    float v = d2 / (d2 - d6);
+    return a + v * ac;  // #5
+  }
+
+  float va = d3 * d6 - d5 * d4;
+  if (va <= 0.f && (d4 - d3) >= 0.f && (d5 - d6) >= 0.f) {
+    float v = (d4 - d3) / ((d4 - d3) + (d5 - d6));
+    return b + v * (c - b);  // #6
+  }
+
+  float denom = 1.f / (va + vb + vc);
+  float v = vb * denom;
+  float w = vc * denom;
+  return a + v * ab + w * ac;  // #0
 }
 
 bool CapsuleTriCheck(glm::vec3 P1, glm::vec3 P2, glm::vec3 P3, glm::vec3 R1,
                      glm::vec3 R2, float radius) {
-  glm::vec3 Normal, IntersectPos;
-
-  return true;
+  int len = (glm::distance(R1, R2) / radius) + 1;
+  for (int i = 0; i < len; i++) {
+    glm::vec3 temp = R1 + (R2 - R1) * (float)i / (float)len;
+    glm::vec3 closest = closestPointTriangle(temp, P1, P2, P3);
+    if (glm::distance(temp, closest) < radius) return true;
+  }
+  return false;
 }
 
 bool movecollisioncheck(glm::vec3 hitbox[], glm::vec3 checkposition,
@@ -81,7 +117,6 @@ bool movecollisioncheck(glm::vec3 hitbox[], glm::vec3 checkposition,
                         Global->Points[Global->mapfaces[i].points[2]],
                         hitbox[0] + checkposition, hitbox[1] + checkposition,
                         radius)) {
-      SDL_Log("%d", i);
       return true;
     }
   }
@@ -97,16 +132,23 @@ void EntityMove(Entity* tempentity) {
   glm::vec3 tempposition = tempentity->position,
             moveresult = glm::vec3({0, 0, 0});
 
-  int temp = (int(std::sqrt(tempmove.x * tempmove.x + tempmove.y * tempmove.y) /
-                  tempentity->hitboxradius) +
-              1);
+  int temp = std::abs(tempmove.x) / tempentity->hitboxradius + 1;
 
   for (int i = 0; i < temp; i++) {
     tempposition.x += tempmove.x / (float)temp;
-    tempposition.y += tempmove.y / (float)temp;
     if (!movecollisioncheck(tempentity->hitbox, tempposition,
                             tempentity->hitboxradius)) {
       moveresult.x += tempmove.x / (float)temp;
+    } else
+      break;
+  }
+
+  temp = std::abs(tempmove.y) / tempentity->hitboxradius + 1;
+  tempposition = moveresult + tempentity->position;
+  for (int i = 0; i < temp; i++) {
+    tempposition.y += tempmove.y / (float)temp;
+    if (!movecollisioncheck(tempentity->hitbox, tempposition,
+                            tempentity->hitboxradius)) {
       moveresult.y += tempmove.y / (float)temp;
     } else
       break;
