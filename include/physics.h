812ae12807b0ba5e5fc54895a810faf6c510380b
Cardboardflavouredbox <minjoon0901@naver.com>
2026-09-01 12:56:49 +0900

    physics libapi thing

--- include/physics.h
+++ include/physics.h
@@ -1,19 +1,32 @@
 #pragma once
 
+#ifdef _WIN32
+#ifdef DLLEXPORT
+#define LIB_API __declspec(dllexport)
+#else
+#define LIB_API __declspec(dllimport)
+#endif
+#else
+#define LIB_API
+#endif
+
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 
 #include "entity.h"
 
-glm::vec3 movecollisioncheck(glm::vec3 hitbox[], glm::vec3 checkposition,
-                             float radius, int teamindex, float& dist,
-                             Entity* tempentity);  // returns the face normal
-struct raycheckresult {
+struct LIB_API raycheckresult {
   glm::vec3 A, B;
   float dist = 0;
 };
 
-raycheckresult capsuleraycheck(glm::vec3 a0, glm::vec3 a1, glm::vec3 b0,
-                               glm::vec3 b1);
+LIB_API raycheckresult capsuleraycheck(glm::vec3 a0, glm::vec3 a1, glm::vec3 b0,
+                                       glm::vec3 b1);
+extern "C" {
+LIB_API glm::vec3 movecollisioncheck(
+    glm::vec3 hitbox[], glm::vec3 checkposition, float radius, int teamindex,
+    float& dist,
+    Entity* tempentity);  // returns the face normal
 
-void EntityMove(Entity* tempentity);
+LIB_API void EntityMove(Entity* tempentity);
+}
